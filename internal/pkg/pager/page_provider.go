95efedf7e75d760451e651079f52c39a5bca3765
hiagobalbino <hiagobalbinoo@gmail.com>
2022-08-13 01:02:20 -0300

    improve page provider test to use http client mock instead of real requests

--- internal/pkg/pager/page_provider.go
+++ internal/pkg/pager/page_provider.go
@@ -2,13 +2,9 @@ package pager
 
 import (
 	"net/http"
-	"sync"
 
 	"github.com/hiago-balbino/web-crawler/internal/core/pager"
 )
-
-var once sync.Once
-var httpClient *http.Client
 
 // PageProvider is a structure with HTTP Client that will be instantiated once using package sync
 type PageProvider struct {
@@ -16,10 +12,7 @@ type PageProvider struct {
 }
 
 // NewPageProvider is a constructor to create a new instance of PageProvider
-func NewPageProvider() pager.Pager {
-	once.Do(func() {
-		httpClient = &http.Client{}
-	})
+func NewPageProvider(httpClient *http.Client) pager.Pager {
 	return PageProvider{httpClient: httpClient}
 }
 
--- internal/pkg/pager/page_provider_test.go
+++ internal/pkg/pager/page_provider_test.go
@@ -1,39 +1,66 @@
 package pager
 
 import (
+	"errors"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/h2non/gock.v1"
 )
 
 func TestPageProvider_Get(t *testing.T) {
 	testCases := []struct {
 		name          string
 		uri           string
+		statusCode    int
 		isExpectedErr bool
+		expectedErr   error
 	}{
 		{
 			name:          "should return error when http connection fails",
-			uri:           "invalid",
+			uri:           "http://invalid.com",
+			statusCode:    http.StatusInternalServerError,
 			isExpectedErr: true,
+			expectedErr:   errors.New("invalid host"),
 		},
 		{
-			name:          "should return response when http connection has success",
-			uri:           "https://google.com",
+			name:          "should return 5xx status code when http connection has valid host but fails",
+			uri:           "http://google.com",
+			statusCode:    http.StatusInternalServerError,
+			isExpectedErr: true,
+			expectedErr:   errors.New("fail to process"),
+		},
+		{
+			name:          "should return 2xx status code when http connection has success",
+			uri:           "http://google.com",
+			statusCode:    http.StatusOK,
 			isExpectedErr: false,
+			expectedErr:   nil,
 		},
 	}
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
-			response, err := NewPageProvider().Get(test.uri)
+			defer gock.Off()
+
+			requestIntercept := gock.New(test.uri)
+			requestIntercept.Reply(test.statusCode)
+			if test.isExpectedErr {
+				requestIntercept.ReplyError(test.expectedErr)
+			}
+			httpClient := &http.Client{Transport: &http.Transport{}}
+			gock.InterceptClient(httpClient)
+
+			response, err := NewPageProvider(httpClient).Get(test.uri)
 
 			if test.isExpectedErr {
-				assert.NotNil(t, err)
+				assert.ErrorIs(t, err, test.expectedErr)
 				assert.Nil(t, response)
 			} else {
-				assert.Nil(t, err)
+				assert.NoError(t, err)
 				assert.NotNil(t, response)
+				assert.Equal(t, test.statusCode, response.StatusCode)
 			}
 		})
 	}
