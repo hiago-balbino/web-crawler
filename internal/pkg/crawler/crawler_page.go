d84af464521659df6acf888b07fb6fdffe534bd1
hiagobalbino <hiagobalbinoo@gmail.com>
2022-08-25 23:41:25 -0300

    implement extract addresses function of the HTML node

--- internal/pkg/crawler/crawler_page.go
+++ internal/pkg/crawler/crawler_page.go
@@ -3,19 +3,42 @@ package crawler
 import (
 	"github.com/hiago-balbino/web-crawler/internal/core/crawler"
 	"github.com/hiago-balbino/web-crawler/internal/core/pager"
+	"golang.org/x/net/html"
+)
+
+const (
+	linkTag  = "a"
+	hrefProp = "href"
 )
 
 // CrawlerPage is a implementation to handle with web crawler
 type CrawlerPage struct {
-	pager.Pager
+	provider pager.Pager
 }
 
 // NewCrawlerPage is a constructor to create a new instance of CrawlerPage
 func NewCrawlerPage(pager pager.Pager) crawler.Crawler {
-	return CrawlerPage{pager}
+	return CrawlerPage{provider: pager}
 }
 
 // Craw execute the call to craw pages
 func (p CrawlerPage) Craw(uri string) {
 	panic("implement me")
+}
+
+// extractAddresses recursively extracts the addresses of the HTML node
+func extractAddresses(links []string, node *html.Node) []string {
+	if node.Type == html.ElementNode && node.Data == linkTag {
+		for _, attr := range node.Attr {
+			if attr.Key == hrefProp {
+				links = append(links, attr.Val)
+			}
+		}
+	}
+
+	for next := node.FirstChild; next != nil; next = next.NextSibling {
+		links = extractAddresses(links, next)
+	}
+
+	return links
 }
--- internal/pkg/crawler/crawler_page_test.go
+++ internal/pkg/crawler/crawler_page_test.go
@@ -1,7 +1,133 @@
 package crawler
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/html"
+)
 
 func TestCrawlerPage_Craw(t *testing.T) {
 
+}
+
+func TestExtractAddresses(t *testing.T) {
+	testCases := []struct {
+		name          string
+		node          *html.Node
+		expectedLinks []string
+	}{
+		{
+			name: "should return link when have only one attribute",
+			node: &html.Node{
+				Type: html.ElementNode,
+				Data: linkTag,
+				Attr: []html.Attribute{{
+					Key: hrefProp,
+					Val: "https://google.com",
+				}},
+			},
+			expectedLinks: []string{"https://google.com"},
+		},
+		{
+			name: "should return link when have more than one attribute",
+			node: &html.Node{
+				Type: html.ElementNode,
+				Data: linkTag,
+				Attr: []html.Attribute{
+					{
+						Key: hrefProp,
+						Val: "https://google.com",
+					},
+					{
+						Key: hrefProp,
+						Val: "https://twitter.com",
+					},
+				},
+			},
+			expectedLinks: []string{"https://google.com", "https://twitter.com"},
+		},
+		{
+			name: "should return only one link when have two attribute but the last is invalid",
+			node: &html.Node{
+				Type: html.ElementNode,
+				Data: linkTag,
+				Attr: []html.Attribute{
+					{
+						Key: hrefProp,
+						Val: "https://google.com",
+					},
+					{
+						Key: "class",
+						Val: "name",
+					},
+				},
+			},
+			expectedLinks: []string{"https://google.com"},
+		},
+		{
+			name: "should return links from parent and child node",
+			node: &html.Node{
+				Type: html.ElementNode,
+				Data: linkTag,
+				Attr: []html.Attribute{
+					{
+						Key: hrefProp,
+						Val: "https://google.com",
+					},
+				},
+				FirstChild: &html.Node{
+					Type: html.ElementNode,
+					Data: linkTag,
+					Attr: []html.Attribute{
+						{
+							Key: hrefProp,
+							Val: "https://twitter.com",
+						},
+					},
+				},
+			},
+			expectedLinks: []string{"https://google.com", "https://twitter.com"},
+		},
+		{
+			name: "should return links from parent and child node when first child also have next sibling",
+			node: &html.Node{
+				Type: html.ElementNode,
+				Data: linkTag,
+				Attr: []html.Attribute{
+					{
+						Key: hrefProp,
+						Val: "https://google.com",
+					},
+				},
+				FirstChild: &html.Node{
+					Type: html.ElementNode,
+					Data: linkTag,
+					Attr: []html.Attribute{
+						{
+							Key: hrefProp,
+							Val: "https://twitter.com",
+						},
+					},
+					NextSibling: &html.Node{
+						Type: html.ElementNode,
+						Data: linkTag,
+						Attr: []html.Attribute{
+							{
+								Key: hrefProp,
+								Val: "https://twitch.com",
+							},
+						},
+					},
+				},
+			},
+			expectedLinks: []string{"https://google.com", "https://twitter.com", "https://twitch.com"},
+		},
+	}
+
+	for _, test := range testCases {
+		links := extractAddresses(nil, test.node)
+
+		assert.ElementsMatch(t, test.expectedLinks, links)
+	}
 }
