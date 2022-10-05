a6508dad1dbfe0519b358a6577a982d6ecc224c0
hiagobalbino <hiagobalbinoo@gmail.com>
2022-10-05 00:06:28 -0300

    add integration test to find stored URIs

--- internal/pkg/crawler/repository/crawler_repository_integration_test.go
+++ internal/pkg/crawler/repository/crawler_repository_integration_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stretchr/testify/suite"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type RepositoryIntegrationTestSuite struct {
@@ -87,8 +88,41 @@ func (suite *RepositoryIntegrationTestSuite) TestInsert() {
 }
 
 func (suite *RepositoryIntegrationTestSuite) TestFind() {
-	err := suite.repository.client.Ping(context.Background(), nil)
-	assert.NoError(suite.T(), err)
+	ctx := context.Background()
+	uri := "http://crawler.com"
+	depth := 1
+	uris := []string{"http://subcrawler.com"}
+
+	suite.Suite.T().Run("should return error to find URIs stored", func(t *testing.T) {
+		defer func() {
+			suite.defaultDBEnviroments()
+		}()
+		viper.Set("MONGODB_DATABASE", "")
+
+		repository := NewCrawlerRepository(ctx)
+		storedURIs, err := repository.Find(ctx, uri, depth)
+
+		assert.NotNil(suite.T(), err)
+		assert.Empty(suite.T(), storedURIs)
+	})
+
+	suite.Suite.T().Run("should return empty slice when try to find URIs stored", func(t *testing.T) {
+		storedURIs, err := suite.repository.Find(ctx, uri, depth)
+
+		assert.EqualError(suite.T(), err, mongo.ErrNoDocuments.Error())
+		assert.Empty(suite.T(), storedURIs)
+	})
+
+	suite.Suite.T().Run("should return stored URIs with success", func(t *testing.T) {
+		dataPage := dataPage{URI: uri, Depth: depth, URIs: uris}
+		_, err := suite.repository.getCollection().InsertOne(ctx, dataPage)
+		assert.NoError(suite.T(), err)
+
+		storedURIs, err := suite.repository.Find(ctx, uri, depth)
+
+		assert.NoError(suite.T(), err)
+		assert.ElementsMatch(suite.T(), uris, storedURIs)
+	})
 }
 
 func (suite *RepositoryIntegrationTestSuite) defaultDBEnviroments() {
