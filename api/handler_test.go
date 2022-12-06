a1e876c4f9180be5abdd7091f915b196c5bf521d
hiagobalbino <hiagobalbinoo@gmail.com>
2022-12-05 23:44:11 -0300

    change test-api to use httpexpect
    > change all test-api to use httpexpect library

--- api/handler_test.go
+++ api/handler_test.go
@@ -1,16 +1,16 @@
 package api
 
 import (
-	"context"
 	"errors"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gavv/httpexpect/v2"
+	"github.com/gin-gonic/gin"
 	"github.com/hiago-balbino/web-crawler/internal/core/crawler"
 	mock "github.com/hiago-balbino/web-crawler/internal/pkg/crawler/mock"
-	"github.com/stretchr/testify/assert"
+	tmock "github.com/stretchr/testify/mock"
 )
 
 func TestGetCrawledPage(t *testing.T) {
@@ -18,101 +18,116 @@ func TestGetCrawledPage(t *testing.T) {
 	givenURI := "https://anyuritest.com"
 
 	t.Run("should return 4xx error", func(t *testing.T) {
+		handler := setupHandler(nil)
+		server := httptest.NewServer(handler)
+		defer server.Close()
+
+		e := httpexpect.Default(t, server.URL)
+
 		t.Run("when empty URI query param", func(t *testing.T) {
-			path := fmt.Sprintf("/crawler?depth=%d", givenDepth)
-			request, err := http.NewRequest(http.MethodGet, path, nil)
-			response := httptest.NewRecorder()
-			assert.NoError(t, err)
-
-			runServerHTTPTest(request, response, nil)
-
-			assert.Equal(t, http.StatusBadRequest, response.Code)
-			assert.Contains(t, response.Body.String(), errEmptyURI.Error())
+			e.GET("/crawler").
+				WithQuery("depth", givenDepth).
+				Expect().
+				Status(http.StatusBadRequest).
+				Body().Contains(errEmptyURI.Error())
 		})
 		t.Run("when empty depth query param", func(t *testing.T) {
-			path := fmt.Sprintf("/crawler?uri=%s", givenURI)
-			request, err := http.NewRequest(http.MethodGet, path, nil)
-			response := httptest.NewRecorder()
-			assert.NoError(t, err)
-
-			runServerHTTPTest(request, response, nil)
-
-			assert.Equal(t, http.StatusBadRequest, response.Code)
-			assert.Contains(t, response.Body.String(), errEmptyDepth.Error())
+			e.GET("/crawler").
+				WithQuery("uri", givenURI).
+				Expect().
+				Status(http.StatusBadRequest).
+				Body().Contains(errEmptyDepth.Error())
 		})
 		t.Run("when negative depth query param", func(t *testing.T) {
-			path := fmt.Sprintf("/crawler?uri=%s&depth=%d", givenURI, -1)
-			request, err := http.NewRequest(http.MethodGet, path, nil)
-			response := httptest.NewRecorder()
-			assert.NoError(t, err)
-
-			runServerHTTPTest(request, response, nil)
-
-			assert.Equal(t, http.StatusBadRequest, response.Code)
-			assert.Contains(t, response.Body.String(), `strconv.ParseUint: parsing &#34;-1&#34;: invalid syntax`)
+			e.GET("/crawler").
+				WithQuery("uri", givenURI).
+				WithQuery("depth", -1).
+				Expect().
+				Status(http.StatusBadRequest).
+				Body().Contains(`strconv.ParseUint: parsing &#34;-1&#34;: invalid syntax`)
 		})
 	})
 
 	t.Run("should return 5xx error when fail to perform HTTP request to fetch page", func(t *testing.T) {
-		path := fmt.Sprintf("/crawler?uri=%s&depth=%d", givenURI, givenDepth)
-		request, err := http.NewRequest(http.MethodGet, path, nil)
-		response := httptest.NewRecorder()
-		assert.NoError(t, err)
-
 		unexpectedErr := errors.New("unexpected error")
 		crawlerService := new(mock.CrawlerServiceMock)
-		crawlerService.On("Craw", context.Background(), givenURI, givenDepth).Return(make([]string, 0), unexpectedErr)
-		runServerHTTPTest(request, response, crawlerService)
+		crawlerService.On("Craw", tmock.Anything, givenURI, givenDepth).Return(make([]string, 0), unexpectedErr)
 
-		assert.Equal(t, http.StatusInternalServerError, response.Code)
-		assert.Contains(t, response.Body.String(), unexpectedErr.Error())
+		handler := setupHandler(crawlerService)
+		server := httptest.NewServer(handler)
+		defer server.Close()
+
+		e := httpexpect.Default(t, server.URL)
+
+		e.GET("/crawler").
+			WithQuery("uri", givenURI).
+			WithQuery("depth", givenDepth).
+			Expect().
+			Status(http.StatusInternalServerError).
+			Body().Contains(unexpectedErr.Error())
 	})
 
 	t.Run("should return 2xx", func(t *testing.T) {
-		path := fmt.Sprintf("/crawler?uri=%s&depth=%d", givenURI, givenDepth)
-		request, err := http.NewRequest(http.MethodGet, path, nil)
-		response := httptest.NewRecorder()
-		assert.NoError(t, err)
-
 		t.Run("when process did not return any results", func(t *testing.T) {
 			links := make([]string, 0)
 			crawlerService := new(mock.CrawlerServiceMock)
-			crawlerService.On("Craw", context.Background(), givenURI, givenDepth).Return(links, nil)
-			runServerHTTPTest(request, response, crawlerService)
+			crawlerService.On("Craw", tmock.Anything, givenURI, givenDepth).Return(links, nil)
 
-			assert.Equal(t, http.StatusOK, response.Code)
-			assert.Contains(t, response.Body.String(), "The process did not return any valid results")
+			handler := setupHandler(crawlerService)
+			server := httptest.NewServer(handler)
+			defer server.Close()
+
+			e := httpexpect.Default(t, server.URL)
+
+			e.GET("/crawler").
+				WithQuery("uri", givenURI).
+				WithQuery("depth", givenDepth).
+				Expect().
+				Status(http.StatusOK).
+				Body().Contains("The process did not return any valid results")
 		})
 		t.Run("when page is successfully crawled", func(t *testing.T) {
 			links := []string{"https://firstlink.com", "https://secondlink.com", "https://thirdlink.com"}
 			crawlerService := new(mock.CrawlerServiceMock)
-			crawlerService.On("Craw", context.Background(), givenURI, givenDepth).Return(links, nil)
-			runServerHTTPTest(request, response, crawlerService)
+			crawlerService.On("Craw", tmock.Anything, givenURI, givenDepth).Return(links, nil)
 
-			assert.Equal(t, http.StatusOK, response.Code)
-			assert.Contains(t, response.Body.String(), links[0])
-			assert.Contains(t, response.Body.String(), links[1])
-			assert.Contains(t, response.Body.String(), links[2])
+			handler := setupHandler(crawlerService)
+			server := httptest.NewServer(handler)
+			defer server.Close()
+
+			e := httpexpect.Default(t, server.URL)
+
+			e.GET("/crawler").
+				WithQuery("uri", givenURI).
+				WithQuery("depth", givenDepth).
+				Expect().
+				Status(http.StatusOK).
+				Body().
+				Contains(links[0]).
+				Contains(links[1]).
+				Contains(links[2])
 		})
 	})
 }
 
 func TestIndex(t *testing.T) {
 	t.Run("should return 2xx when load index page", func(t *testing.T) {
-		path := "/index"
-		request, err := http.NewRequest(http.MethodGet, path, nil)
-		response := httptest.NewRecorder()
-		assert.NoError(t, err)
+		handler := setupHandler(nil)
+		server := httptest.NewServer(handler)
+		defer server.Close()
 
-		runServerHTTPTest(request, response, nil)
+		e := httpexpect.Default(t, server.URL)
 
-		assert.Equal(t, http.StatusOK, response.Code)
+		e.GET("/index").
+			Expect().
+			Status(http.StatusOK)
 	})
 }
 
-func runServerHTTPTest(request *http.Request, response *httptest.ResponseRecorder, service crawler.CrawlerService) {
+func setupHandler(service crawler.CrawlerService) *gin.Engine {
 	handler := NewHandler(service)
 	server := Server{handler: handler}
 	router := server.setupRoutes("../templates/*")
-	router.ServeHTTP(response, request)
+
+	return router
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hiago-balbino/web-crawler
 go 1.19
 
 require (
+	github.com/gavv/httpexpect/v2 v2.6.1
 	github.com/gin-gonic/gin v1.8.1
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.12.0
@@ -17,6 +18,8 @@ require (
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/Microsoft/go-winio v0.5.2 // indirect
 	github.com/Microsoft/hcsshim v0.9.4 // indirect
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
 	github.com/containerd/cgroups v1.0.4 // indirect
 	github.com/containerd/containerd v1.6.8 // indirect
@@ -24,6 +27,7 @@ require (
 	github.com/docker/docker v20.10.17+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -33,15 +37,19 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/imkira/go-interpol v1.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/moby/sys/mount v0.3.3 // indirect
 	github.com/moby/sys/mountinfo v0.6.2 // indirect
@@ -56,6 +64,8 @@ require (
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sanity-io/litter v1.5.5 // indirect
+	github.com/sergi/go-diff v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
@@ -63,10 +73,18 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.34.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	github.com/yudai/gojsondiff v1.0.0 // indirect
+	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
@@ -79,6 +97,7 @@ require (
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	moul.io/http2curl/v2 v2.3.0 // indirect
 )
 
 require (
